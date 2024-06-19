bc1e607167a78f20ab7a9872ed80bcc47fc5b26c
PonchoBT <ponchobt.dev@gmail.com>
2024-06-18 22:19:37 -0600

    redux

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,9 @@
 class Todo{
   constructor(todo, id){
-    this.title = todo.title;
+    this.name = todo.name;
     this.order = todo.order;
     this.completed = false;
+    this.toppings = todo.toppings;
     this.id = id;
   }
 }
